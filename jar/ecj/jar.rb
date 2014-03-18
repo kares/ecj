e1dce079b31dbfb63e0be5ea62b10639ac3fc42c
kares <self@kares.org>
2014-03-18 20:39:56 +0100

    need ecj 3.x as well - go with the last in the 3 series: 3.8.1

--- jar/ecj/jar.rb
+++ jar/ecj/jar.rb
@@ -1,5 +1,5 @@
 module ECJ
-  JAR_VERSION = '4.3.1'
+  JAR_VERSION = '3.8.2'
   JAR = File.expand_path("../../jar/ecj-#{JAR_VERSION}.jar", File.dirname(__FILE__))
 
   def self.load_jar; load(JAR) end
